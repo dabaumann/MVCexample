925dab5d8ae62aa4e5b90e17bb954ddb720aefa1
Daniel Baumann <baumannda@gmail.com>
2019-11-24 18:09:18 -0600

    added controllers

---
+++ MVCexample/Controllers/AuthorizationController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCexample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+      
+    }
+}
